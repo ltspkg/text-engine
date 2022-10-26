91f12d1109f5f0526d9762a00b9517f26451cf04
chunsen.zhou <chunsen.zhou@foxmail.com>
2022-10-26 22:14:30 +0800

    Zhoucs624 patch 1 (#3)
    docs: create static.yml and setup docs

---
+++ docs/counter.ts
@@ -0,0 +1,30 @@
+import { textEngineFactory } from '../src'
+
+const textEngine = textEngineFactory({
+  renders: {
+    a1(text) {
+      return `<div style="color: red">${text}</div>`;
+    }
+  },
+  transfers: {
+    b1(s1): number {
+      return Number(s1);
+    },
+    b2(s2: number) {
+      return Math.floor(s2 /1000) + ''
+    }
+  }
+})
+
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0
+  const setCounter = (count: number) => {
+    counter = count
+
+    const result = textEngine.parse('hahaha#a1{36000}b1,b2#');
+
+    window.console.log(result);
+    element.innerHTML = result;
+  }
+  element.addEventListener('click', () => setCounter(++counter))
+}
